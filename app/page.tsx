bdc92c247b8972e7e111c00d6386e991de24bc94
Alain Bonneau <alaindev97@gmail.com>
2026-03-02 02:39:32 +0100

    feat/ add how it work section

--- app/page.tsx
+++ app/page.tsx
@@ -19,8 +19,43 @@ export default function Home() {
           height={458}
         />
       </div>
+
       {/* Composant qui affiche la grille de propriétés */}
-      <PropertyGrid />
+      <section className="home-page-property-grid">
+        <PropertyGrid />
+      </section>
+
+      {/* Section "Comment ça marche ?" */}
+      <section className="how-work">
+        <h2>Comment ça marche ?</h2>
+        <p>
+          Que vous partiez pour un week-end improvisé, des vacances en famille
+          ou un voyage professionnel, Kasa vous aide à trouver un lieu qui vous
+          ressemble.
+        </p>
+        <div className="how-container">
+          <div className="how-item">
+            <h4>Recherchez</h4>
+            <p>
+              Entrez votre destination, vos dates et laissez Kasa faire le reste
+            </p>
+          </div>
+          <div className="how-item">
+            <h4>Réservez</h4>
+            <p>
+              Profitez d’une plateforme sécurisée et de profils d’hôtes
+              vérifiés.
+            </p>
+          </div>
+          <div className="how-item">
+            <h4>Vivez l&apos;expérience</h4>
+            <p>
+              Installez-vous, profitez de votre séjour, et sentez-vous chez
+              vous, partout.
+            </p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
