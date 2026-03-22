4a1692c05176a924dac3afd9a34642ade180e200
Alain Bonneau <alaindev97@gmail.com>
2026-03-22 02:20:44 +0100

    feat/ add loader to property picture component

--- app/properties/[id]/components/PropertyPicture/PropertyPicture.tsx
+++ app/properties/[id]/components/PropertyPicture/PropertyPicture.tsx
@@ -1,4 +1,8 @@
+"use client";
+
+import { useState } from "react";
 import Image from "next/image";
+import Loader from "@/app/components/ui/Loader/Loader";
 import "./PropertyPicture.scss";
 
 const BACKEND_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
@@ -18,38 +22,55 @@ export default function PropertyPicture({
   cover?: string;
   pictures: string[];
 }) {
+  const [mainLoaded, setMainLoaded] = useState(false);
+  const [loadedImages, setLoadedImages] = useState<Record<number, boolean>>({});
+
   if (!pictures || pictures.length === 0) {
     return null;
   }
 
-  if (!cover) {
-    cover = pictures[0];
-  }
+  const mainPicture = cover || pictures[0];
 
   return (
     <div className="property-picture-container">
       <div className="picture-left">
+        {!mainLoaded && <Loader />}
+
         <Image
-          src={getImageUrl(cover)}
+          src={getImageUrl(mainPicture)}
           alt="Photo principale du logement"
-          className="property-picture-main"
+          className={`property-picture-main ${mainLoaded ? "is-visible" : "is-hidden"}`}
           fill
           sizes="100vw"
           style={{ objectFit: "cover" }}
           unoptimized
+          onLoad={() => setMainLoaded(true)}
         />
       </div>
 
       <div className="picture-right">
         {pictures.map((picture, index) => (
           <div key={index} className="small-picture-wrapper">
+            {!loadedImages[index] && (
+              <div className="picture-loader">
+                <Loader />
+              </div>
+            )}
+
             <Image
               src={getImageUrl(picture)}
               alt={`Photo ${index + 1} du logement`}
+              className={loadedImages[index] ? "is-visible" : "is-hidden"}
               fill
               sizes="50vw"
               style={{ objectFit: "cover" }}
               unoptimized
+              onLoad={() =>
+                setLoadedImages((prev) => ({
+                  ...prev,
+                  [index]: true,
+                }))
+              }
             />
           </div>
         ))}
